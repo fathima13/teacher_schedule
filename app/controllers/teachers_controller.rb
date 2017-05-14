a5179e082188413fc7ccbff8a77e05223d43c3c8
Vivek <vivekpatna52@gmail.com>
2017-05-14 12:43:47 +0530

    refactor

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,48 +1,47 @@
 class TeachersController < ApplicationController
-   before_action :set_teacher, only: [:show, :edit, :update, :destroy]
+  before_action :set_teacher, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @teachers=Teacher.all
-      if params[:q].present?
-        @teachers=Teacher.where("name ilike ?","%#{params[:q]}%" ).order("name asc")
-      elsif params[:search].present?
-        @start_date  = (Date.civil(params[:teacher]["start_date(1i)"].to_i,params[:teacher]["start_date(2i)"].to_i,params[:teacher]["start_date(3i)"].to_i)).strftime("%Y-%m-%d")
-        @end_date  = (Date.civil(params[:teacher]["end_date(1i)"].to_i,params[:teacher]["end_date(2i)"].to_i,params[:teacher]["end_date(3i)"].to_i)).strftime("%Y-%m-%d")
-        if(params[:sortway]=="Order By Desc")
-          @teachers = Teacher.search(params.require(:search)).where("dob>? and dob<?",@start_date,@end_date).order("dob desc")
-        else
-          params
-          @teachers = Teacher.search(params.require(:search)).where("dob>? and dob<?",@start_date,@end_date).order("dob asc")
-        end
-      end
-    end
+  def index
+    @teachers = Teacher.search index_params
+  end
   
   def new
   	@teacher=Teacher.new
   end
-   def create
+
+  def create
   	@teacher=Teacher.new(teacher_params)
   	@teacher.save
     redirect_to teachers_url
   end
+
   def edit
-  	end
+  end
+
   def update
      @teacher.update(teacher_params)
-  	@teacher.save
-  	redirect_to teachers_url
-  end
+     @teacher.save
+     redirect_to teachers_url
+   end
+  
   def show
-  	  end
-   def destroy
-  	@teacher.destroy
-  	redirect_to teachers_url
   end
+  
+  def destroy
+   @teacher.destroy
+   redirect_to teachers_url
+  end
+ 
   def set_teacher
-      @teacher = Teacher.find(params[:id])
-    end
+    @teacher = Teacher.find(params[:id])
+  end
+
   def teacher_params
-      params.require(:teacher).permit(:name, :gender, :dob)
-    end
-   
- end
+    params.require(:teacher).permit(:name, :gender, :dob)
+  end
+
+  def index_params
+    params.permit(:q, :start_date, :end_date, :order_by_field, :order_by)
+  end
+
+end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,27 @@
 class Teacher < ApplicationRecord
+
+  GENDERS = [ "male", "female" ]
+  ORDER_BY_FIELDS = [ "name", "dob", "gender" ]
+  ORDER_BY = [ "Asc", "Desc" ]
+
 	has_many :schedules
-	def self.search(search)
-  where("name LIKE ? OR gender LIKE ? ", "%#{search}%", "%#{search}%") 
-end
+
+	def self.search params
+    teachers = self
+    if params[:q].present?
+      teachers = teachers.where("name ilike ?", "%#{params[:q]}%")
+    end
+    if params[:start_date].present?
+      teachers = teachers.where("dob > ?", params[:start_date])
+    end
+    if params[:end_date].present?
+      teachers = teachers.where("dob < ?", params[:end_date])
+    end
+    if params[:order_by_field].present?
+      order_by = params[:order_by] || "asc"
+      order_by_field = params[:order_by_field]
+      teachers = teachers.order("#{order_by_field} #{order_by}")
+    end
+    teachers
+  end
 end
